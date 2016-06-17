042228a6edfb9c21d76dff2792d0436947000d97
Sharon's Mac <help@pencilevent.com>
2016-06-17 12:00:10 -0400

    Added Landscape Left and Landscape Right to supported views.

--- BlueButterflyRedButterfly/ViewController.swift
+++ BlueButterflyRedButterfly/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueButterfly: UIImageView!
+    @IBOutlet weak var blueButterflyWidth: NSLayoutConstraint!
+    @IBOutlet weak var blueButterflyHeight: NSLayoutConstraint!
+    @IBOutlet weak var redButterfly: UIImageView!
+    @IBOutlet weak var hideBlueButterflyButton: UIButton!
+    @IBOutlet weak var hideRedButterflyButton: UIButton!
+    
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +31,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlueButterfly(sender: AnyObject) {
+     
+        if blueButterfly.hidden {
+            blueButterfly.hidden = false
+            hideBlueButterflyButton.setTitle("Hide Blue Butterfly", forState: .Normal)
+        
+        } else {
+            blueButterfly.hidden = true
+            hideBlueButterflyButton.setTitle("Show Blue Butterfly", forState: .Normal)
+        }
+        
+    }
+    
+    
+    @IBAction func hideRedButterfly(sender: AnyObject) {
+        if redButterfly.hidden {
+            hideRedButterflyButton.setTitle("Hide Red Butterfly", forState: .Normal)
+            
+        } else {
+            hideRedButterflyButton.setTitle("Show Red Butterfly", forState: .Normal)
+            
+        }
+        redButterfly.hidden = !redButterfly.hidden
+        // ask Nic to remind you about this line
+    }
 
-}
+    @IBAction func flyAwayAction(sender: AnyObject) {
+        UIView.animateWithDuration(2) {
+            self.topConstraint.constant = 400
+            self.leadingConstraint.constant = 400
+            self.blueButterflyWidth.constant = 75
+            self.blueButterflyHeight.constant = 50
+            self.view.layoutIfNeeded()
+
+        }
+        
+        
+        
+        }
+    }
+
 
